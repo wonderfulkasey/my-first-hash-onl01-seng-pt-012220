91e5b224896dabf7735bfb33e320f5c3b0165a99
Kasey Arnold <kaseyarnold15@gmail.com>
2020-01-31 01:36:19 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -40,7 +40,7 @@ def adding
                  }
   # add 2 muskets to the shipping_manifest hash below
 
-shipping_manifest
+shipping_manifest["muskets"]=
 
   # add 4 gun powder to the shipping_manifest hash below
 
